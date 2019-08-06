f5b477a9c48e855bebb78f02e288832187794694
606195 <patriciaburns@C02YG09AJGH7.local>
2019-08-06 14:17:30 -0400

    init commit:

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express'); 
+const app = express(); 
+const PORT = process.env.PORT || 5000; 
+
+app.get('/', (req, res) => {
+    res.send(
+        'API RUNNING'
+    )
+})
+
+
+app.listen(PORT, ()=> {
+    console.log( `server started on port + ${PORT} `)
+} )
+
